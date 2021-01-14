7407365bad220a822f79e2e423248e7542518028
Fahad Shaikh <mfahadfff@gmail.com>
2021-01-14 17:01:56 -0600

    Initial FrontEnd Commit

---
+++ tinder-clone/src/TinderCards.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
+
+function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+      name: "Elon Musk",
+      url:
+        "https://robbreport.com/wp-content/uploads/2020/08/elon-musk.jpg?w=1000",
+    },
+    {
+      name: "Jeff Bezos",
+      url:
+        "https://content.fortune.com/wp-content/uploads/2020/02/GettyImages-1078542150.jpg?resize=1500,1000",
+    },
+  ]);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen");
+  };
+
+  return (
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TinderCards;
